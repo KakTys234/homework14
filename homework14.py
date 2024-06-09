79ac27188eb6ad2d5e32ac300155db6d3e9b021f
KakTys234 <168638151+KakTys234@users.noreply.github.com>
2024-06-09 23:22:24 +0300

    Add files via upload

--- homework14.py
+++ homework14.py
@@ -1,5 +1,10 @@
 class House:
-    def __init__(self):
-        self.numberOfFloors = 0
-    def setNewNumberOfFloors(floors):
-        print(numberOfFloors)
+    def __init__(self, nameHouse, numberOfFloors):
+        self.nameHouse = nameHouse
+        self.numberOfFloors = numberOfFloors
+
+    def setNewNumberOfFloors(self):
+        print(f'Название: {self.nameHouse}, Этажей: {self.numberOfFloors}')
+
+Orion = House('Орион', 23)
+Object = House('Объект', 25)
